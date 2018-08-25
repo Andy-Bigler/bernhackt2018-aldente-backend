0de49885746560ce443f142be7feeffc22c898a1
Andy Bigler <andy.bigler@besonet.ch>
2018-08-25 19:12:04 +0200

    Added properties for challenges

--- lib/models/challenge.ts
+++ lib/models/challenge.ts
@@ -7,12 +7,14 @@ export default class Challenge extends bookshelf.Model<Challenge> {
     id: number;
     name: string;
     description: string;
+    points: number;
     time?: number;
     distance?: number;
-    vheicle?: string;
+    vehicle?: string;
     special?: string;
     from?: string;
     to?: string;
+    isAutomatic: boolean
 
     get tableName() { return 'challenges'; }
     get hasTimestamps() { return true; }
--- seeds/challenges.js
+++ seeds/challenges.js
@@ -4,7 +4,9 @@ exports.seed = function(knex, Promise) {
       .then(function () {
         return Promise.all([
           // Inserts seed entries
-          knex('challenges').insert({id: 1, name: 'Test'})
+          knex('challenges').insert({id: 1, name: 'Linie 10x10', description: 'Fahre 10 Stationen mit der Linie 10', points: 100, time: null, distance: null, vehicle: null, special: null, from: null, to: null, isAutomatic: true}),
+          knex('challenges').insert({id: 2, name: '10km Fahren', description: 'Fahre 10km im Berner öV Netz', points: 500, time: null, distance: 10, vehicle: null, special: null, from: null, to: null, isAutomatic: true}),
+          knex('challenges').insert({id: 3, name: 'Dampftram Challenge', description: 'Fahre mindestens eine Stunde im Dampftram', points: 100, time: null, distance: null, vehicle: 'Tram', special: 'Dampftram', from: null, to: null, isAutomatic: false})
         ]);
       });
   };
