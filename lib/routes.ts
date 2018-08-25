ca023f14f2c82df0fbdabf73e79477e56dd52156
Andy Bigler <andy.bigler@besonet.ch>
2018-08-25 11:30:04 +0200

    Added Http Service

--- lib/routes.ts
+++ lib/routes.ts
@@ -2,11 +2,12 @@ import * as express from 'express';
 import {Request, Response} from 'express';
 
 import Challenge from './models/challenge';
+import HttpRequest from './helpers/httpRequest';
 
 export class Routes {       
     public routes(app: express.Application): void {          
         app.route('/')
-        .get((req: Request, res: Response) => {            
+        .get((req: Request, res: Response) => {
             res.status(200).send({
                 message: 'GET request successfulll!!!!'
             })
