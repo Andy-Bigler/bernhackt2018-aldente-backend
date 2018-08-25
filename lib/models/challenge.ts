994d7cce0f32d3cc89bc701e6f222b995938d61a
Andy Bigler <andy.bigler@besonet.ch>
2018-08-25 17:57:16 +0200

    Added Map

--- lib/models/challenge.ts
+++ lib/models/challenge.ts
@@ -6,6 +6,13 @@ var bookshelf = db.getBookshelf();
 export default class Challenge extends bookshelf.Model<Challenge> {
     id: number;
     name: string;
+    description: string;
+    time?: number;
+    distance?: number;
+    vheicle?: string;
+    special?: string;
+    from?: string;
+    to?: string;
 
     get tableName() { return 'challenges'; }
     get hasTimestamps() { return true; }
--- lib/models/vehicle.ts
+++ lib/models/vehicle.ts
@@ -1,5 +1,4 @@
 var protobuf = require('protobufjs')
-var HttpRequest = require('')
 
 export default class Vehicle {
     static allVehicles () {
