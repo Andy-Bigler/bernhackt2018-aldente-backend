97cb353dff7167a70f84f830c3aa3eb5dc7d049b
Wolf Benjamin <Benjamin.Wolf@swisscom.com>
2018-08-25 19:14:48 +0200

    Add cors to express server so frontend can fetch from backend

--- lib/app.ts
+++ lib/app.ts
@@ -2,6 +2,14 @@ import * as express from "express";
 import * as bodyParser from "body-parser";
 import { Routes } from "./routes";
 
+const allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'localhost:8080');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+  next();
+}
+
 class App {
   public app: express.Application;
   public route: Routes = new Routes();
@@ -15,6 +23,7 @@ class App {
   private config(): void {
     this.app.use(bodyParser.json());
     this.app.use(bodyParser.urlencoded({ extended: false }));
+    this.app.use(allowCrossDomain);
   }
 }
 
