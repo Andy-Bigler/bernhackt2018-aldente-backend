016f282982dc192332990c9fffe5563bae69535f
Wolf Benjamin <Benjamin.Wolf@swisscom.com>
2018-08-25 20:10:25 +0200

    Correct cors header so frontend can fetch data from api

--- lib/app.ts
+++ lib/app.ts
@@ -3,7 +3,7 @@ import * as bodyParser from "body-parser";
 import { Routes } from "./routes";
 
 const allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'localhost:8080');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
   res.header('Access-Control-Allow-Methods', 'GET,PUT');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
 
